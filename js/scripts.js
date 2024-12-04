09b7f4b1fd6f7e8c31d5e67bd10e4b00f96b8bf8
Siyanda <siyandaxaba1@gmail.com>
2024-12-04 15:33:51 +0200

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,33 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker(addressCoords).addTo(map)
   .bindPopup('82 Diakonia Avenue, Durban')
   .openPopup();
+ 'use strict';
+
+// Function to type out text
+function typeText(element, texts, index = 0, textIndex = 0) {
+    if (index < texts.length) {
+        const text = texts[index];
+        if (textIndex < text.length) {
+            element.textContent += text.charAt(textIndex);
+            textIndex++;
+            setTimeout(() => typeText(element, texts, index, textIndex), 100);
+        } else {
+            setTimeout(() => {
+                element.textContent = ''; // Clear the text
+                typeText(element, texts, index + 1); // Move to the next text
+            }, 2000); // Wait before clearing the text
+        }
+    }
+}
+
+// Start typing effect
+const typingElement = document.getElementById('typing-text');
+const sentences = [
+  "I am a Junior Full Stack Developer.",
+    "I am a software developer.",
+    "I am a web developer.",
+    "I am proficient with Azure.",
+    "I am a Database Administrator."
+];
+
+typeText(typingElement, sentences);
